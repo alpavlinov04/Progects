3127103bb53bc759dd565dfe1049e2e8c2a7cc76
alpavlinov04 <alekspav2004@gmail.com>
2021-10-11 16:54:05 +0300

    Add files

--- aplication.php
+++ aplication.php
@@ -4,68 +4,37 @@
 </head>
 <body>
 <center>
-<?php
-$Name = $Email = $gender = $comment = $Website = "";
+  <?php
+  $Name = $Email =  $Password = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST")
-{
-  $Name = test_input($_POST["Name"]);
-  $Email = test_input($_POST["Email"]);
-  $Website = test_input($_POST["Website"]);
-  $comment = test_input($_POST["comment"]);
-  $gender = test_input($_POST["gender"]);
-}
+  if ($_SERVER["REQUEST_METHOD"] == "POST")
+  {
+    $Name = test_input($_POST["Name"]);
+    $Email = test_input($_POST["Email"]);
+    $Password = test_input($_POST["comment"]);
+  }
 
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-session_set_cookie_params('1200');
-session_start();
-$_SESSION[$Name] = $_POST["Name"];
-$_SESSION[$Email] = $_POST["Email"];
-echo $_POST["Name"];
-echo "<br>";
-echo $_POST["Email"];
+  function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
 
-?>
+  ?>
 
-<h2>PHP Form Validation</h2>
 <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
   Name: <input type="text" name="Name">
   <br><br>
   E-mail: <input type="text" name="Email">
   <br><br>
-  Website: <input type="text" name="Website">
-  <br><br>
-  Comment: <textarea name="comment" rows="5" cols="40"></textarea>
-  <br><br>
-  Gender:
-  <input type="radio" name="gender" value="Female">Female
-  <input type="radio" name="gender" value="Male">Male
-  <input type="radio" name="gender" value="Other">Other
+  Password: <input type= "password" name = "Password">
   <br><br>
   <input type="submit" name="submit" value="Submit">
   <br><br>
-  <a href="index.php">My first php</a>
+  <a href="magazin.php">Magazin</a>
 </form>
 
-<?php
-echo "<h2>Your Input:</h2>";
-echo $Name;
-echo "<br>";
-echo $Email;
-echo "<br>";
-echo $Website;
-echo "<br>";
-echo $comment;
-echo "<br>";
-echo $gender;
-echo "<br>";
-echo "<br>";
-?>
 </center>
 </body>
 </html>
---
+++ magazin.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+  <style>
+  body {background-color: white;}
+  h1 {color: black; text-align: center; font-family: cursive; font-size: 160%;}
+  p {color: black; text-align: left;; font-family: cursive; font-size: 100%; text-align: center;}
+  .button {
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+  }
+  .button1 {background-color: red;}
+  </style>
+  <meta charset="UTF-8">
+  <title>Alexis</title>
+</head>
+<body>
+  <a class="button button1" href="aplication.php">Register form</a>
+
+  <h1>Alexis onlain shop!</h1>
+  <center>
+  <img src="Alex.jpg" alt="Alex logo" width="200" height="200">
+</center>
+  <p></p>
+  <p>Men's winter jacket</p>
+  <img src="Jacket.jpg" alt="Men's winter jacket" width="auto" height="auto">
+  <p></p>
+  <p>Very good men's winter jacket. Ideal for all winter conditions. With additional sweatshirt. Internal pocket. Ideally up to -15 degrees.</p>
+  <p>Price BGN 50.</p>
+  <p></p>
+  <p>Female's winter jacket</p>
+  <img src = "Jacket2.jpg" alt = "Female winter jacket" width = "auto" height="auto">
+  <p></p>
+  <p>Very good Female winter jacket. Ideal for all winter conditions. With additional sweatshirt. Internal pocket. Ideally up to -15 degrees.</p>
+  <p>Price BGN 55.</p>
+  <p></p>
+  <p>Men's T-shirt</p>
+  <img src="T-shirt.jpg" alt  = "Men's T-shirt" width = "auto" height="auto">
+  <p></p>
+  <p>Very good men's t-shirt. With print 'A.V.E.R.I'. 100% cotton.</p>
+  <p>Price BGN 8.</p>
+  <p></p>
+  <p>Female T-shirt</p>
+  <img src="T-shirt2.jpg" alt  = "Female T-shirt" width = "auto" height="auto">
+  <p></p>
+  <p>Very good female t-shirt. With print 'be happy'. 100% cotton.</p>
+  <p>Price BGN 9.</p>
+  <p></p>
+  <p>Girl's T-shirt</p>
+  <img src="T-shirt3.jpg" alt  = "Female T-shirt" width = "auto" height="auto">
+  <p></p>
+  <p>Very good girl's t-shirt. With print. 100% cotton.</p>
+  <p>Price BGN 5.</p>
+  <p></p>
+</body>
+</html>
